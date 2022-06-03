175e42d24ceb703a7d270e1d7c0b7962d1c8c329
rizkadelysalsabilah <106718850+rizkadelysalsabilah@users.noreply.github.com>
2022-06-03 23:46:34 +0700

    Update struktur-zeolite.cpp

--- struktur-zeolite.cpp
+++ struktur-zeolite.cpp
@@ -11,3 +11,10 @@ int main (){
     // deklarasi variabel
     float pi = 3.14;
     int counter = 0;
+
+// deklarasi struktur-zeolit
+float Cx[125], Cy[125], Cz[125]
+float H1x[125], H1y[125], H1z[125]
+float H2x[125], H2y[125], H2z[125]
+float H3x[125], H3y[125], H3z[125]
+float H4x[124], H4y[125], H4z[125]
