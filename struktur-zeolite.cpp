fd15f39a2bef91248671932734f32b9e00c3718b
rizkadelysalsabilah <106718850+rizkadelysalsabilah@users.noreply.github.com>
2022-06-04 13:59:37 +0700

    Update struktur-zeolite.cpp

--- struktur-zeolite.cpp
+++ struktur-zeolite.cpp
@@ -34,23 +34,23 @@ float O4x[125], O4y[125], O4z[125]
     ofstream file;
     file.open("struktur-zeolite.xyz");
     file<<"3\n"<< endl;
-    file<<setw(3)<<"Al1"<<setw(3)<< " "\
-    <<fixed<<setprecision(3)<<Al1x<<setw(3)<<" "\
-    <<fixed<<setprecision(3)<<Al1y<<setw(3)<<" "\
-    <<fixed<<setprecision(3)<<Al1z<<setw(3)<<" "\n;
+    file<<setw(3)<<"Al"<<setw(3)<< " "\
+    <<fixed<<setprecision(3)<<Alx<<setw(3)<<" "\
+    <<fixed<<setprecision(3)<<Aly<<setw(3)<<" "\
+    <<fixed<<setprecision(3)<<Alz<<setw(3)<<" "\n;
     
-    file<<setw(3)<<"Al2"<<setw(3)<< " "\
-    <<fixed<<setprecision(3)<<Al2x<<setw(3)<<" "\
-    <<fixed<<setprecision(3)<<Al2y<<setw(3)<<" "\
-    <<fixed<<setprecision(3)<<Al2z<<setw(3)<<" "\n;
+    file<<setw(3)<<"Si"<<setw(3)<< " "\
+    <<fixed<<setprecision(3)<<Six<<setw(3)<<" "\
+    <<fixed<<setprecision(3)<<Siy<<setw(3)<<" "\
+    <<fixed<<setprecision(3)<<Siz<<setw(3)<<" "\n;
     
-    file<<setw(3)<<"Al3"<<setw(3)<< " "\
-    <<fixed<<setprecision(3)<<Al3x<<setw(3)<<" "\
-    <<fixed<<setprecision(3)<<Al3y<<setw(3)<<" "\
-    <<fixed<<setprecision(3)<<Al3z<<setw(3)<<" "\n;
+    file<<setw(3)<<"O1"<<setw(3)<< " "\
+    <<fixed<<setprecision(3)<<Olx<<setw(3)<<" "\
+    <<fixed<<setprecision(3)<<O1y<<setw(3)<<" "\
+    <<fixed<<setprecision(3)<<O1z<<setw(3)<<" "\n;
     
-    file<<setw(3)<<"Al4"<<setw(3)<< " "\
-    <<fixed<<setprecision(3)<<Al4x<<setw(3)<<" "\
-    <<fixed<<setprecision(3)<<Al4y<<setw(3)<<" "\
-    <<fixed<<setprecision(3)<<Al4z<<setw(3)<<" "\n;
+    file<<setw(3)<<"O2"<<setw(3)<< " "\
+    <<fixed<<setprecision(3)<<O2x<<setw(3)<<" "\
+    <<fixed<<setprecision(3)<<O2y<<setw(3)<<" "\
+    <<fixed<<setprecision(3)<<O2z<<setw(3)<<" "\n;
     file << "3\n" << endl;
