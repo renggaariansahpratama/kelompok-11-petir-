cddb872b73c8a883ce5343ac556cfbe5ebb6b4b2
rizkadelysalsabilah <106718850+rizkadelysalsabilah@users.noreply.github.com>
2022-06-16 07:26:06 +0700

    Update struktur-zeolite.cpp

--- struktur-zeolite.cpp
+++ struktur-zeolite.cpp
@@ -59,47 +59,74 @@ float O4x[125], O4y[125], O4z[125]
     float rB_O4y = rB_Aly;
     float rB_O4z = rB_Alz;
     
-    // memasukkan nilai tersebut ke dalam file
-    ofstream file;
-    file.open("struktur-zeolite.xyz");
-    file<<"3\n"<< endl;
-    file<<setw(3)<<"Al"<<setw(3)<< " "\
-    <<fixed<<setprecision(3)<<Alx<<setw(3)<<" "\
-    <<fixed<<setprecision(3)<<Aly<<setw(3)<<" "\
-    <<fixed<<setprecision(3)<<Alz<<setw(3)<<" "\n;
+    float rB_Alx ;counter;rB_Alx + (i*3,01)
+    float rB_Aly ;counter;rB_Aly + (j*3,01)
+    float rB_Alz ;counter;rB_Alz + (k*3,01)
     
-    file<<setw(3)<<"Si"<<setw(3)<< " "\
-    <<fixed<<setprecision(3)<<Six<<setw(3)<<" "\
-    <<fixed<<setprecision(3)<<Siy<<setw(3)<<" "\
-    <<fixed<<setprecision(3)<<Siz<<setw(3)<<" "\n;
+    float rB_six ;counter;rB_six ;counter + rB_six;
+    float rB_siy ;counter;rB_siy ;counter + rB_siy;
+    float rB_siz ;counter;rB_siz ;counter + rB_siz;
     
-    file<<setw(3)<<"O1"<<setw(3)<< " "\
-    <<fixed<<setprecision(3)<<Olx<<setw(3)<<" "\
-    <<fixed<<setprecision(3)<<O1y<<setw(3)<<" "\
-    <<fixed<<setprecision(3)<<O1z<<setw(3)<<" "\n;
+    float rB_O1x ;counter;rB_six ;counter + rB_O1x;
+    float rB_O1y ;counter;rB_siy ;counter + rB_O2y;
+    float rB_O1z ;counter;rB_siz ;counter + rB_O1z;
     
-    file<<setw(3)<<"O2"<<setw(3)<< " "\
-    <<fixed<<setprecision(3)<<O2x<<setw(3)<<" "\
-    <<fixed<<setprecision(3)<<O2y<<setw(3)<<" "\
-    <<fixed<<setprecision(3)<<O2z<<setw(3)<<" "\n;
+    float rB_O2x ;counter;rB_six ;counter + rB_O2x;
+    float rB_O2y ;counter;rB_siy ;counter + rB_O2y;
+    float rB_O2z ;counter;rB_siz ;counter + rB_O2z;
     
-    file<<setw(3)<<"O3"<<setw(3)<< " "\
-    <<fixed<<setprecision(3)<<O3x<<setw(3)<<" "\
-    <<fixed<<setprecision(3)<<O3y<<setw(3)<<" "\
-    <<fixed<<setprecision(3)<<O3z<<setw(3)<<" "\n;
+    float rB_O3x ;counter;rB_six ;counter + rB_O3x;
+    float rB_O3y ;counter;rB_siy ;counter + rB_O3y;
+    float rB_O3z ;counter;rB_siz ;counter + rB_O3z;
+    
+    float rB_O4x ;counter;rB_six ;counter + rB_O4x;
+    float rB_O4y ;counter;rB_siy ;counter + rB_O4y;
+    float rB_O4z ;counter;rB_siz ;counter + rB_O4z;
+    
+    counter += 1;
+   }
+}
+
+    
    
-    file<<setw(3)<<"O4"<<setw(3)<< " "\
-    <<fixed<<setprecision(3)<<O4x<<setw(3)<<" "\
-    <<fixed<<setprecision(3)<<O4y<<setw(3)<<" "\
-    <<fixed<<setprecision(3)<<O4z<<setw(3)<<" "\n;
-    file << "3\n" << endl;
     
-    file.close();
     
-    cout<<"program selesai";
     
-    return 0;
-  }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+   
+    
+    
 
 
 float densitas;          /* densitas ini memiliki satuan g cm^-3
